19ed4ff60da3bbc6d5ed44648dadff53bf65ec91
David Smith <dave@rabbit.beer>
2024-10-16 10:09:08 -0400

    changed get fetch route app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const fetchBooks = async () => {
     try {
-      const response = await fetch("http://crimetea.com/api/books/");
+      const response = await fetch("http://127.0.0.1:8000/api/books/");
       const data = await response.json();
       //console.log("response content-type: " + response.headers.get("content-type"));
       //console.log("response headers: " + response.headers.get("content-type"));
